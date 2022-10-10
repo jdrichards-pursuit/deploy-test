f5bd62f8b589fa4c952f8c024924f521c442857a
jd richards <jdrichardstech@gmail.com>
2022-10-10 16:25:08 -0400

    check auto deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Pursuit App Template</h1>
       <h2>This is a test Netlify App</h2>
+      <h3>Continuous deployment is automatic now</h3>
     </div>
   );
 }
