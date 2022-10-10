c0812d9a9b752b5a033bb8e7f686e21607d64158
jd richards <jdrichardstech@gmail.com>
2022-10-10 18:30:32 -0400

    remove file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import Extra from './components/Extra';
+
 import './App.css';
 
 function App() {
-  console.log('secret', process.env.REACT_APP_SECRET);
+  console.log('secret', process.env.REACT_APP_API_KEY);
   return (
     <div className="App">
       <h1>Pursuit App Template</h1>
       <h2>This is a test Netlify App</h2>
       <h3>Continuous deployment is automatic now</h3>
+      <Extra />
     </div>
   );
 }
