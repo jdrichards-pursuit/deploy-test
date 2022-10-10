25652f091f0e0cb26d74bb4cefb54f0ba42e107f
jd richards <jdrichardstech@gmail.com>
2022-10-10 18:34:43 -0400

    fix broken component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Extra from './components/Extra';
-
 import './App.css';
 
 function App() {
@@ -9,7 +7,6 @@ function App() {
       <h1>Pursuit App Template</h1>
       <h2>This is a test Netlify App</h2>
       <h3>Continuous deployment is automatic now</h3>
-      <Extra />
     </div>
   );
 }
